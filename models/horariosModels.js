e5698eb01fc5c7fd5f6fe7f5d92a76a227458d6c
alexuma <m5a97b@gmail.com>
2022-02-17 13:13:19 -0300

    funcionalidad modificar horarios

--- models/horariosModels.js
+++ models/horariosModels.js
@@ -1,9 +1,24 @@
 var pool = require('./bd')
 
 async function getHorarios(){
-    var query = 'select * from horarios'
-    var rows = await pool.query(query)
-    return rows
+    var query = 'select * from horarios';
+    var rows = await pool.query(query);
+    return rows;
 }
 
-module.exports = {getHorarios}
+async function getDiasById(id){
+    var query = 'SELECT * FROM horarios WHERE id = ?';
+    var rows = await pool.query(query,[id]);
+    return rows[0];
+}
+
+async function editarHorarioById(obj,id){
+    try {
+        var query = 'UPDATE horarios SET ? WHERE id=?';
+        var rows = await pool.query(query,[obj,id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+module.exports = {getHorarios , getDiasById , editarHorarioById}
--- routes/admin/cambios.js
+++ routes/admin/cambios.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var horariosModel = require('../../models/horariosModels');
 
+/* Listado de horarios*/
 router.get('/', async function (req,res,next){
 
     var horarios = await horariosModel.getHorarios();
@@ -11,5 +12,37 @@ router.get('/', async function (req,res,next){
         horarios
     })
 });
+
+/* Vista del formulario Horarios para editar*/
+
+router.get('/horarios/:id',async(req,res,next)=>{
+    var id=req.params.id;
+    var horario = await horariosModel.getDiasById(id)
+    res.render('admin/horarios',{
+        layout:'admin/layout',
+        horario
+    })
+
+})
+
+/* Para UPDATE*/
+router.post('/horarios', async (req,res,next)=>{
+    try {
+        var obj={
+            horarios:req.body.horarios
+        }
+        
+        await horariosModel.editarHorarioById(obj,req.body.id)
+        res.redirect('/admin/cambios');
+
+    } catch (error) {
+        console.log(error);
+        res.render('admin/horarios',{
+            layout:'admin/layout',
+            error:true,
+            message:'No se modifico la novedad'
+        })
+    }
+})
 
 module.exports = router;
